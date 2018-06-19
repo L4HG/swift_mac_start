8627b63fdac08b268bc643c2b8bbebb539035ae3
l4hg <namezis@yandex.ru>
2018-06-19 10:59:12 +0300

    Initial Commit

---
+++ swift_mac_start/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift_mac_start
+//
+//  Created by Kirill Ovchinnikov on 19/06/2018.
+//  Copyright © 2018 Kirill Ovchinnikov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
